7d0f34380ade809ded24fdb98d7a349b9bd81ca9
sohithsai777 <sohithd696@gmail.com>
2024-11-13 22:44:03 +0000

    Set up Test1

--- app.py
+++ app.py
@@ -101,5 +101,7 @@ def upload_file():
 
     return render_template('index.html', images=image_data_list, bucket_name=BUCKET_NAME)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
